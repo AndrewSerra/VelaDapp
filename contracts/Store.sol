6c4169e011bf0833714bfb524d7e401decc5ef9b
Andrew Serra <andy@serra.us>
2021-05-11 23:37:54 -0400

    created a initial structure for the store contract 2

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.0;
 
 /*
-@title A store contract to manage coupon sales
-@author Vela
-*/
+ * @title A store contract to manage coupon sales
+ * @author Vela
+ */
 contract Store {
     string private name;
     uint private couponSaleCount = 0;
     mapping (uint => CouponSale) public couponSales;
 
-    // 
+    // Coupon Sales that keep a collection of coupons available
     struct CouponSale {
         uint id;
         string description;
@@ -21,13 +21,42 @@ contract Store {
         Coupon[] coupons;
     }
 
+    // Individual coupons for a sale collection
     struct Coupon {
         uint id;
         bool purchased;
         address payable owner;
     }
 
+    // Create new coupons sales group
+    event CouponSalesCreated(
+        uint id,
+        string description,
+        uint worth,
+        uint price,
+        uint availabilityCount,
+        bool active,
+        Coupon[] coupons
+    );
+
+    // End of coupon sales group
+    event CouponSalesEnd(
+        uint id,
+        string description,
+        bool active
+    );
+
+    /*
+     * @descr Contructor of the Store, manage coupon sales
+     * @param _name - Name of the store creating the contract
+     */
     constructor(string memory _name) {
         name = _name;
     }
+
+    function getActiveSalesCount() external view returns (uint) {
+        return couponSaleCount;
+    }
+
+
 }
